db91526baedc3cce2a3b55c4d0e03d15cd6f4562
Pavukoplov <arachnoplov@gmail.com>
2022-11-10 23:32:23 +0300

    Добавлено столкновение круга героя с врагом.

--- core/src/game/accelewarrior/Warrior.java
+++ core/src/game/accelewarrior/Warrior.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
 
 public class Warrior {
@@ -24,14 +25,15 @@ public class Warrior {
         warriorTexture = new Texture(textureWarriorPath);
         circleTexture = new Texture(textureCirclePath);
         square = new Rectangle(356, 187, 50, 50);
-        circle = new Circle(356, 187, radius);
+        circle = new Circle();
+        circle.radius = radius;
     }
 
     public void render(SpriteBatch batch) {
         batch.draw(warriorTexture, square.getX(), square.getY(), square.getHeight(), square.getWidth());
         if (Gdx.input.isTouched()) {
-            circle.x = square.x - halfRadius;
-            circle.y = square.y - halfRadius;
+            circle.x = square.x + square.getWidth() / 2;
+            circle.y = square.y + square.getHeight() / 2;
             batch.draw(circleTexture, square.x - halfRadius,
                     square.y - halfRadius,
                     diameter, diameter);
@@ -57,6 +59,13 @@ public class Warrior {
         }
 
         if (square.overlaps(game.getFoe().getSquareFoe())) {
+            square.setX(356);
+            square.setY(187);
+            game.getFoe().getSquareFoe().setX(700);
+            game.getFoe().getSquareFoe().setY(70);
+        }
+
+        if (Intersector.overlaps(circle, game.getFoe().getSquareFoe())) {
             square.setX(356);
             square.setY(187);
             game.getFoe().getSquareFoe().setX(700);
